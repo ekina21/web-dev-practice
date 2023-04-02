8cb0ee06e11b2a17aa9497c7bc56c8a272fdc6de
ekina21 <126476941+ekina21@users.noreply.github.com>
2023-04-02 12:58:12 +0545

    hieeet

--- sign-up/script.js
+++ sign-up/script.js
@@ -3,25 +3,48 @@ let errors={};
 
 submitButton.addEventListener("click",function(e){
     e.preventDefault();
+    errors={};
     let signupform=document.forms["signupform"]; 
-    let FirstnameInput=signupform.Firstname;
-    let lastnameInput=signupform.Lastname;
+    let firstNameInput=signupform.firstName;
+    let lastNameInput=signupform.lastName;
+    let EmailInput=signupform.Email;
+    let passwordInput=signupform.password;
     
-    if(FirstnameInput.value==""){
-        errors.Firstname="firstname should not be empty"
+    if(firstNameInput.value==""){
+        errors.firstName="firstname should not be empty"
        // let firstnameGroup=document.getElementById('firstnameGroup');
        // let firstnameMessage=document.querySelector("#firstnameGroup .message");
         //firstnameGroup.classList.add("error");
        // firstnameMessage.textContent="firstname should not be empty";
 
     }
-    if(lastnameInput.value==""){
-        errors.lastname="lastname should not be empty"
+    if(lastNameInput.value==""){
+        errors.lastName="lastname should not be empty";
         //let laststnameGroup=document.getElementById('lastnameGroup');
         //let lastnameMessage=document.querySelector("#lastnameGroup .message");
         //lastnameGroup.classList.add("error");
-       // lastnameMessage.textContent="lastname should not be empty";
+      // lastnameMessage.textContent="lastname should not be empty";
     }
-    console.log(errors)
-    for(let error in errors){}
+    if(EmailInput.value==""){
+        errors.Email="Email should not be empty";
+    }
+    if(passwordInput.value==""){
+        errors.password="password should not be empty";
+    }
+    if(passwordInput.value.length<=5){
+        errors.password="minimum 6 character are needed";
+    }
+    console.log(errors);
+    for(let error in errors){
+        let lastNameGroup=document.getElementById(`${error}Group`);
+        let lastNameMessage=document.querySelector(`#${error}Group .message`);
+        lastNameGroup.classList.add("error");
+       lastNameMessage.textContent=errors[error];
+    
+    }
+if(Object.keys(errors).length==0){
+    signupform.submit();
+}
+    
+    
 });
